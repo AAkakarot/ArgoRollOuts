5330db249b106109bb4d55d9ff4c70890f583919
AAkakarot <akashsingh0536@gmail.com>
2024-10-02 18:35:12 +0530

    add go sum

--- app/main.go
+++ app/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "time"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-    log.Printf("%v : Starting application ", time.Now())
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Printf(w, "Router Working")
-    })
+	log.Printf("%v : Starting application ", time.Now())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Router Working")
+	})
 
-    fmt.Println("Starting server on :8080...")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println("Error starting server:", err)
-    }
+	fmt.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run(":8080") // Default listens on :8080
 }
